094efa84867d442355347dc6fd4f7b064eb1b3c6
ilushaputi <putintzeff.ilya@yandex.com>
2022-04-04 21:31:23 +0300

    Lesson 5 Update employee in database

--- src/main/java/com/ilyaputintsev/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/ilyaputintsev/spring/mvc_hibernate_aop/controller/MyController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.jws.WebParam;
 import java.util.List;
@@ -38,8 +39,16 @@ public class MyController {
     public String saneEmployee(@ModelAttribute("employee") Employee employee){
         employeeService.saveEmployee(employee);
 
-
         return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model){
+
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee",employee);
+
+        return "employee-info";
     }
 
 }
